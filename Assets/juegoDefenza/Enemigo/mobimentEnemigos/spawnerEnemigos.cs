464a90e5eb2c5c60f6b910d911244dbbfd81b094
xxx2131N <165327222+xxx2131N@users.noreply.github.com>
2026-03-11 02:54:54 -0300

    se aplico las partes que se nececitaban
    hecho

--- Assets/juegoDefenza/Enemigo/mobimentEnemigos/spawnerEnemigos.cs
+++ Assets/juegoDefenza/Enemigo/mobimentEnemigos/spawnerEnemigos.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using System.Collections.Generic;
 
-public class spawnerEnemigos : MonoBehaviour
+public class SpawnEnemigos : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public List<GameObject> enemigos; // lista de enemigos
+
+    public List<Transform> camino1;
+    public List<Transform> camino2;
+    public List<Transform> camino3;
+
+    public Transform puntoSpawn;
+
     void Start()
     {
-        
+        InvokeRepeating("CrearEnemigo", 1f, 3f);
     }
 
-    // Update is called once per frame
-    void Update()
+    void CrearEnemigo()
     {
-        
+        // elegir enemigo random
+        int enemigoRandom = Random.Range(0, enemigos.Count);
+
+        GameObject enemigo = Instantiate(enemigos[enemigoRandom], puntoSpawn.position, Quaternion.identity);
+
+        // elegir camino random
+        int caminoRandom = Random.Range(1, 4);
+
+        mobimentenemi script = enemigo.GetComponent<mobimentenemi>();
+
+        if (caminoRandom == 1)
+        {
+            script.puntosDeRuta = camino1;
+        }
+        else if (caminoRandom == 2)
+        {
+            script.puntosDeRuta = camino2;
+        }
+        else
+        {
+            script.puntosDeRuta = camino3;
+            Destroy(gameObject); // se destruye al llegar al final
+        }
     }
 }
